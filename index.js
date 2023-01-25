7321e74e628003cc5411d5ede36269a7a80f7b5f
Kween <kweenyoallhiada@gmail.com>
2023-01-24 21:28:30 -0800

    Add home and guest functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function addOnePointHome() {}
+function addTwoPointsHome() {}
+function addThreePointsHome() {}
+
+addOnePointHome()
+addTwoPointsHome()
+addThreePointsHome()
+
+function addOnePointGuest() {}
+function addTwoPointsGuest() {}
+function addThreePointsGuest() {}
+
+addOnePointGuest()
+addTwoPointsGuest()
+addThreePointsGuest()
